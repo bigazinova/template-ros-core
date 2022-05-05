8db14e89bc7946e9e3beef8d96f0870083e02cc8
Sergey Glazunov <31547459+light5551@users.noreply.github.com>
2022-05-05 19:38:31 +0300

    Update circle_drive.py

--- packages/circle_drive/scripts/circle_drive.py
+++ packages/circle_drive/scripts/circle_drive.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python3
+import sys
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import Twist2DStamped # v, omega
 
+class MyNode(DTROS):
+  
+    def __init__(self, node_name):
+        super(MyNode, self).__init__(node_name=node_name, node_type=NodeType.DEBUG)
+        self.pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
+        
+    def run(self):
+        while not rospy.is_shutdown():
+            msg = Twist2DStamped()
+            msg.v = 0.0
+            msg.omega = 1.0
+            self.pub.publish(msg)
+
+if __name__ == "__main__":
+    node = MyNode(node_name="circle_drive_node")
+    
+    node.run()
+    
+    rospy.spin()
