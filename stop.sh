4d8a32d0c3b18e5dceb0c59f444c64c3cbbb05d8
Sergey Glazunov <31547459+light5551@users.noreply.github.com>
2022-07-16 17:52:11 +0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,1 +1,2 @@
-rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "NORMAL_JOYSTICK_CONTROL"}'
+#rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "NORMAL_JOYSTICK_CONTROL"}'
+rostopic pub -r 10 /{hostname}/joy sensor_msgs/Joy '{header:{},axes:[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],buttons:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}'
