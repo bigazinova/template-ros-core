a7f3531d3fee80f06ab9fe276e0f67122ee0543c
bigazinova <108048920+bigazinova@users.noreply.github.com>
2022-07-19 13:55:08 +0300

    Update solution.py

--- packages/circle_drive/scripts/solution.py
+++ packages/circle_drive/scripts/solution.py
@@ -1,4 +1,14 @@
 
 def solution(obs):
+    hsv = cv.cvtColor(obs,cv.COLOR_BGR2HSV)
+    lower = np.array([20,100,100])
+    upper = np.array([30,255,255])
+    mask = cv.inRange(hsv,lower,upper)
+    ratio = (cv.countNonZero(mask))/(obs.size/3)
+    res = np.round(ratio*100,2)
     print(obs.shape)
-    return [0, 1]
+    
+    if res >= 2:
+        return [1,0]
+    else:
+        return [0,1]
